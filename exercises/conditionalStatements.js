ec4712cc9b387a4f382c54e15600cbd918ec4ead
Patricio <concertmate@btinternet.com>
2020-05-04 17:25:34 +0100

    new commit

--- exercises/conditionalStatements.js
+++ exercises/conditionalStatements.js
@@ -1,8 +1,29 @@
 let sale=true;
-//sale=false;
+let hungerLevel=8;
 
+//sale=false;
+//part one: If statement
 if(sale){
   console.log('Time to buy!');
 }else{
   console.log('Time to wait for a sale');
+}
+
+//part two: Comparison Operators
+
+if(hungerLevel > 7){
+  console.log('Time to eat!')
+}else{
+    console.log('We can eat later!');
+}
+
+//part 3 logical operators
+let mood='sleepy'
+let tirednessLevel=6;
+
+
+if(mood === 'sleepy' && tirednessLevel > 8){
+  console.log('time to sleep');
+}else{
+  console.log('not bedtime yet!');
 }
